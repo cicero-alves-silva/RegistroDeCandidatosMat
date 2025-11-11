f5ed16f4c134f936aa7f411af48f89e16c768c0e
CICERO <cicero.alves@ifpb.edu.br>
2025-11-11 12:37:17 -0300

    Aula 11/11/2025

--- app/src/main/java/com/cicero/registrodecandidatosmat/MainActivity.kt
+++ app/src/main/java/com/cicero/registrodecandidatosmat/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.cicero.registrodecandidatosmat
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.cicero.registrodecandidatosmat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fabAdicionar.setOnClickListener { view ->
+            //Abrir tela de cadastro
+            val intent = Intent(this, CadastroCandidatoActivity::class.java)
+            startActivity(intent)
         }
     }
 }
