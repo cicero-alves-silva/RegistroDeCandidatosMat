809a0819ffb72a922b836f8b3edbb428b348d139
CICERO <cicero.alves@ifpb.edu.br>
2025-11-18 12:30:39 -0300

    Aula 18/11/2025 - parte 2

--- app/src/main/java/com/cicero/registrodecandidatosmat/CandidatoAdapter.kt
+++ app/src/main/java/com/cicero/registrodecandidatosmat/CandidatoAdapter.kt
@@ -1,24 +1,44 @@
 package com.cicero.registrodecandidatosmat
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.cicero.registrodecandidatosmat.CandidatoAdapter.CandidatoViewHolder
 import com.cicero.registrodecandidatosmat.databinding.ItemCandidatoBinding
 
 class CandidatoAdapter(var candidatoDAO: CandidatoDAO) :
-    RecyclerView.Adapter<CandidatoViewHolder>()  {
+    RecyclerView.Adapter<CandidatoViewHolder>() {
     private var listaCandidatos = mutableListOf<Candidato>()
 
     inner class CandidatoViewHolder(var binding: ItemCandidatoBinding) :
         RecyclerView.ViewHolder(binding.root) {
+        fun bind(posicao: Int) {
+            var candidato = listaCandidatos[posicao]
+            binding.tvNomeUrna.text = candidato.nomeNaUrna
+            binding.tvNumero.text = candidato.numeroCandidato.toString()
+            binding.tvCargo.text = candidato.cargoCandidato
+            binding.tvPartido.text = candidato.nomePartido
+
+            binding.btEditar.setOnClickListener { view ->
+            }
+
+            binding.btExcluir.setOnClickListener { view ->
+            }
+        }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CandidatoViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+            CandidatoViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemCandidatoBinding.inflate(layoutInflater, parent, false)
+        return CandidatoViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: CandidatoViewHolder, position: Int) {
+        holder.bind(position)
     }
 
     override fun getItemCount(): Int {
+        return listaCandidatos.size
     }
 }
